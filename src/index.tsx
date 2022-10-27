70b829f7f79bb6c91b827d4e4c0f7b2acf4f5fa3
Nicolas Ovejero <nicolas.ovejero.epitech@gmail.com>
2022-10-27 11:40:51 -0400

    Feat: Add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import reduxStore from './store/store'; 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0BnCnFbrMYNqt0LJG1egSlRSjBb06GKc",
+  authDomain: "wiki-it-f9d1e.firebaseapp.com",
+  projectId: "wiki-it-f9d1e",
+  storageBucket: "wiki-it-f9d1e.appspot.com",
+  messagingSenderId: "810287948791",
+  appId: "1:810287948791:web:5fb94bf090aedbea9984f2"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
